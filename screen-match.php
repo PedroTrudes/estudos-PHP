64f5afce29af02c1fa3e2ae33262523dccb76381
PedroTrudes <pedro.jtrudes@gmail.com>
2024-09-24 18:32:58 -0300

    :tada: commit inicial

---
+++ screen-match.php
@@ -0,0 +1,10 @@
+<?php
+
+$nomeFilme = "Top Gun - Maverick";
+$anoLancamento = 2023;
+$notaFilme = 8.9;
+$incluidoNoPlano = true;
+$lista = [1,3,4,];
+
+echo "Bem vindo ao ScrenMatch!\n";
+echo "Lista de filmes disponiveis: ".$nomeFilme. "\nAno Lancamento: " . $anoLancamento;
